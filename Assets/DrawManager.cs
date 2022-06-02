af2602c777b5b7787f12fcf97066a2da442bc651
BlackPenguin <dkstmdwnswns@naver.com>
2022-06-02 19:23:37 +0900

    DrawManager Import

--- Assets/DrawManager.cs
+++ Assets/DrawManager.cs
@@ -1,17 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class DrawManager : MonoBehaviour
 {
     [SerializeField] bool onMouseClick;
     [SerializeField] ParticleSystem particle;
-    Image curImage;
+    [SerializeField] GameObject Brush;
+
     void Update()
     {
-        MouseInputManager();
+        MouseInput();
         MouseParticle();
+    }
+    void MouseInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            onMouseClick = true;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            onMouseClick = false;
+        }
+    }
+    void onBrushing()
+    {
+        Instantiate(Brush, Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity);
     }
     void MouseParticle()
     {
@@ -20,36 +35,5 @@ public class DrawManager : MonoBehaviour
         else if (particle.isPlaying && !onMouseClick)
             particle.Stop();
         particle.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + Vector3.forward * 10;
-    }
-    void MouseInputManager()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            onMouseClick = true;
-            RaycastHit2D[] objs = Physics2D.RaycastAll(Camera.main.WorldToScreenPoint(Input.mousePosition), Vector3.forward);
-            foreach (var obj in objs)
-            {
-                if (obj.transform.CompareTag("StartPoint"))
-                {
-                    curImage = obj.transform.GetChild(0).GetComponent<Image>();
-                }
-            }
-        }
-        if (Input.GetMouseButtonUp(0) && onMouseClick)
-        {
-            RaycastHit2D[] objs = Physics2D.RaycastAll(Camera.main.WorldToScreenPoint(Input.mousePosition), Vector3.forward);
-            foreach (var obj in objs)
-            {
-                if (obj.transform.CompareTag("EndPoint"))
-                {
-                    curImage = obj.transform.GetChild(0).GetComponent<Image>();
-                }
-            }
-            onMouseClick = false;
-        }
-    }
-    void nowStart()
-    {
-
     }
 }
